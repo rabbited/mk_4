74443fcaa0abf9dc049d231e5a5641f503cf2f5d
v <v@vs-MacBook-Pro.local>
2025-03-12 16:52:33 +0000

    modified header logo

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Entry file
+  entry: './src/index.js', // Your main JavaScript file
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Webpack will generate bundle.js here
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Clears old files in 'dist' when rebuilding
   },
   mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, '.'), // Serve from the root folder
+    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Tells Webpack where your HTML file is
+      filename: 'index.html',
+    }),
+  ],
 };
